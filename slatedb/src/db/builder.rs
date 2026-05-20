4292b0210f4b0983cbc55fac3e72b99cc44e5fc8
Ryan Dielhenn <dielhennr@gmail.com>
2026-05-20 01:03:37 -0700

    linter caught unused arg

--- slatedb/src/db/builder.rs
+++ slatedb/src/db/builder.rs
@@ -1151,8 +1151,6 @@ impl<P: Into<Path>> CompactorBuilder<P> {
             self.system_clock,
             self.closed_result,
             self.merge_operator,
-            #[cfg(feature = "compaction_filters")]
-            self.compaction_filter_supplier,
         )
     }
 
